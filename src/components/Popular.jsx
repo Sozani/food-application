d5d0c96d2534eda3401a65d5e6245fd9b72c12d7
sozani <sys612603@gmail.com>
2023-02-04 01:48:35 +0200

    change

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Popular() {
   const getPopular = async () => {
-    const api = await fetch('https://api.spoonacular.com/recipes/random?apiKey=${process.env.React-APP-API-KEY}')
+    const api = await fetch('https://api.spoonacular.com/recipes/random?apiKey=${process.env.React-APP-API-KEY}&number=9')
   }
   return <div>Popular</div>;
 }
