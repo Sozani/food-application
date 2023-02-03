9ca45cf093baa2bc9c5a80dab19691635812abb5
sozani <sys612603@gmail.com>
2023-02-04 01:25:18 +0200

    api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Popular() {
+  const getPopular = async () => {
+    const api = await fetch('https://api.spoonacular.com/recipes/random?apiKey=${process.env.React-APP-API-KEY}')
+  }
   return <div>Popular</div>;
 }
 
