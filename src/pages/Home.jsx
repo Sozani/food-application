b23b6b1fe6cf951e77a9aae506b6c1a3f702b643
sozani <sys612603@gmail.com>
2023-02-04 00:34:06 +0200

    delete some code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
-import Veggie from "./components/";
 import Popular from "../components/Popular";
 import Veggie from "../components/Veggie";
+
+import React from "react";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+export default Home;
