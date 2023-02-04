bffe957918fc8ed5b3eac83682d652b52edc959b
sozani <sys612603@gmail.com>
2023-02-04 03:30:28 +0200

    products

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect,useState } from "react";
 
 function Popular() {
+  const[popular,setPopular]=useState([])
   useEffect(() => {
     getPopular()
   },[])
   const getPopular = async () => {
     const api = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
     const data = await api.json()
-    console.log(data)
+   console.log(data)
+    setPopular(data.products)
+   
   }
   return <div>Popular</div>;
 }
