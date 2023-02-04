d89660611878f834182e242b27d792cdb4326b4d
sozani <sys612603@gmail.com>
2023-02-04 04:09:11 +0200

    change

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,18 +1,25 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Popular() {
-  const[popular,setPopular]=useState([])
+  const [popular, setPopular] = useState([]);
   useEffect(() => {
-    getPopular()
-  },[])
+    getPopular();
+  }, []);
   const getPopular = async () => {
-    const api = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
-    const data = await api.json()
-   console.log(data)
-    setPopular(data.products)
-   
-  }
-  return <div>Popular</div>;
+    const api = await fetch(
+      `http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`
+    );
+    const data = await api.json();
+    console.log(data);
+    setPopular(data.products);
+  };
+  return (
+    <div>
+      {popular.map((product) => {
+        return <div key={product.id}>{product.brand}</div>;
+      })}
+    </div>
+  );
 }
 
 export default Popular;
